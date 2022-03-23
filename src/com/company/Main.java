e40d9fa96ae35df38a94dc0746e6b2b5cf5da767
Reznik Kirill <plotnego.store@gmail.com>
2022-03-23 13:18:23 +0200

    task_2(refactored)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,27 +1,34 @@
 package com.company;
 
 public class Main {
-
+    //сделана проверка в конструкторе , в сеттерах , в функции смены имени и в функции вывода.
     public static void main(String[] args) {
 
-            Person p1 = new Person();
-            p1.setFirstName("Iskander");
-            p1.setLastName("Bombulaev");
-            p1.setBirthYear(2000);
+        Person p1 = new Person();
+        p1.setFirstName("Iskander");
+        p1.setLastName("Bombulaev");
+        p1.setBirthYear(2000);
 
-            Person p2 = new Person("Ono","Uno");
-            p2.setBirthYear(1999);
+//Этот код не пройдет проверку.
+//        p1.setFirstName("Iska_nder");
+//        p1.setLastName("Bombul;aev");
+//        p1.setBirthYear(0);
+//        Person p2 = new Person("On3o", "Uno");
+//        p2.setBirthYear(2023);
 
-            Person p3 = new Person("Andy","Exil");
-            p3.setBirthYear(1997);
+        Person p2 = new Person("Ono", "Uno");
+        p2.setBirthYear(1999);
 
-            Person p4 = new Person("Alex","Rayn");
-            p4.setBirthYear(1998);
+        Person p3 = new Person("Andy", "Exil");
+        p3.setBirthYear(1997);
 
-            Person p5 = new Person();
-            p5.input();
+        Person p4 = new Person("Alex", "Rayn");
+        p4.setBirthYear(1998);
 
-        p1.changedName("Neiskander","Nebombulaev");
+        Person p5 = new Person();
+        p5.input();
+
+        p1.changedName("Neiskander", "Nebombulaev");
 
         System.out.println(p1.info());
         System.out.println(p2.info());
--- src/com/company/Person.java
+++ src/com/company/Person.java
@@ -1,42 +1,85 @@
 package com.company;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Person {
     private String firstName,
-                    lastName;
+            lastName;
     private int birthYear;
 
-    public Person(){
+    public Person() {
 
     }
-    public Person(String firstName , String lastName){
-        this.firstName=firstName;
-        this.lastName=lastName;
+
+    public Person(String firstName, String lastName) {
+        try {
+            if (!exCheckName(firstName) || !exCheckName(lastName))
+                throw new InputMismatchException("Введены некорректные данные при создании объекта");
+            {
+                this.firstName = firstName;
+                this.lastName = lastName;
+            }
+        } catch (InputMismatchException e) {
+            System.err.println(e.getMessage());
+        }
     }
 
-    public int getAge(){
-        int Age = 2022-birthYear;
+    public int getAge() {
+        int Age = 2022 - birthYear;
         return Age;
     }
 
-    public void input(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите имя : ");
-        this.firstName = scanner.nextLine();
-        System.out.println("Введите фамилию : ");
-        this.lastName = scanner.nextLine();
-        System.out.println("Введите возраст : ");
-        this.birthYear = scanner.nextInt();
+    private boolean exCheckAge(String sc) {
+        Pattern pattern = Pattern.compile("\\d{1,2}");
+        Matcher matcher = pattern.matcher(sc);
+        return matcher.matches();
     }
 
-    public String output(){
-        return "Имя : "+firstName+"\nОтчество : "+lastName+"\nГод рождения : "+birthYear;
+    private boolean exCheckName(String sc) {
+//        Pattern pattern = Pattern.compile("[a-zA-Zа-яА-я]{2,}");
+        Pattern pattern = Pattern.compile("^[A-ZА-Я][a-zа-я]+");
+        Matcher matcher = pattern.matcher(sc);
+        return matcher.matches();
     }
 
-    public void changedName(String fn, String ln){
-        this.firstName = fn;
-        this.lastName = ln;
+    public void input() {
+        try (Scanner scanner = new Scanner(System.in);) {
+            System.out.println("Введите имя : ");
+            String tempfirstname = scanner.nextLine();
+            if (!exCheckName(tempfirstname)) throw new InputMismatchException("Введены некорректные данные");
+            else this.firstName = tempfirstname;
+            System.out.println("Введите фамилию : ");
+            String templastname = scanner.nextLine();
+            if (!exCheckName(templastname)) throw new InputMismatchException("Введены некорректные данные");
+            else this.lastName = templastname;
+            System.out.println("Введите возраст : ");
+            String tempage = scanner.nextLine();
+            if (!exCheckAge(tempage)) throw new InputMismatchException("Введены некорректные данные");
+            else this.birthYear = Integer.parseInt(tempage);
+
+        } catch (InputMismatchException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    public String output() {
+        return "Имя : " + firstName + "\nОтчество : " + lastName + "\nГод рождения : " + birthYear;
+    }
+
+    public void changedName(String fn, String ln) {
+        try {
+            if (!exCheckName(fn) || !exCheckName(ln))
+                throw new InputMismatchException("Введены некорректные данные при смене имени");
+            {
+                this.firstName = fn;
+                this.lastName = ln;
+            }
+        } catch (InputMismatchException e) {
+            System.err.println(e.getMessage());
+        }
     }
 
     public String getFirstName() {
@@ -52,20 +95,44 @@ public class Person {
     }
 
     public void setBirthYear(int birthYear) {
-        this.birthYear = birthYear;
+
+        try {
+            if (birthYear <= 0 || birthYear > 2022)
+                throw new InputMismatchException("Введен некорректный год /Setter " + birthYear);
+            this.birthYear = birthYear;
+        } catch (InputMismatchException e) {
+            System.err.println(e.getMessage());
+        }
     }
 
     public void setFirstName(String firstName) {
-        this.firstName = firstName;
+        try {
+            if (!exCheckName(firstName))
+                throw new InputMismatchException("Введено некорректное имя /Setter " + firstName);
+            else this.firstName = firstName;
+        } catch (InputMismatchException e) {
+            System.err.println(e.getMessage());
+        }
+
     }
 
     public void setLastName(String lastName) {
-        this.lastName = lastName;
+        try {
+            if (!exCheckName(lastName))
+                throw new InputMismatchException("Введена некорректная фамилия /Setter " + lastName);
+            else this.lastName = lastName;
+        } catch (InputMismatchException e) {
+            System.err.println(e.getMessage());
+        }
     }
 
-    public String info(){
+    public String info() {
+        if (!(firstName == null || lastName == null || birthYear == 0))
+            return output() + "\nВозраст : " + getAge();
 
-
-        return output()+"\nВозраст : "+getAge();
+        return "Объект заполнен не полностью";
     }
+
+    public static final String ANSI_YELLOW = "\u001b[33m";
+
 }
