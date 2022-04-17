01f66c09495a173585a7cb8e27db9531dac2a65d
Reznik Kirill <plotnego.store@gmail.com>
2022-04-17 16:08:06 +0300

    Lesson_12

---
+++ src/com/company/Main.java
@@ -0,0 +1,69 @@
+package com.company;
+
+import java.io.BufferedReader;
+
+import java.io.InputStreamReader;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.TemporalAdjusters;
+import java.util.*;
+
+
+public class Main {
+
+    public static void Sort(List<String> stringList){
+        Collections.sort(stringList, String::compareTo);
+    }
+    public static void main(String[] args) {
+
+//        System.out.println("Enter n");
+//        Random random = new Random();
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))){
+            System.out.println("Enter n");
+            Random random = new Random();
+            int n = Integer.parseInt(br.readLine());
+            if(n<=0) throw new InputMismatchException("zero or negative");
+//Закомментировал первые выводы т.к. захотел работать с одним списком рандомных чисел для наглядности.
+        // 1
+//            random.ints().limit(n).forEach(System.out::println);
+        // 2
+//            random.ints().limit(n).sorted().forEach(System.out::println);
+
+            //3
+            //Вопрос , как можно по другому записать в коллекцию рандомные числа?
+            // Можно ли через стрим?
+            List<Integer> integerList =  new ArrayList<>(n);
+            for (int i = 0; i < n; i++) {
+                integerList.add(i, random.nextInt());
+            }
+            System.out.println("Original");
+            integerList.stream().forEach(System.out::println);
+
+            System.out.println("Sorted");
+//Вопрос : какой вариант сортировки оптимальней? Collections.sort(integerList); или ↓
+            integerList.stream().sorted().forEach(System.out::println);
+
+//вариант поиска мах 1
+            IntSummaryStatistics statistics = integerList.stream().mapToInt(x->x).summaryStatistics();
+            System.out.println("max1 ="+statistics.getMax());
+//вариант поиска мах 2
+            Optional<Integer> max2 = integerList.stream().max(Comparator.naturalOrder());
+            System.out.println("max2 ="+max2.get());
+//вариант поиска мах 3 какой лучше? последние два видимо почти одно и тоже.
+            Optional<Integer> max3 = integerList.stream().max((x,y)->x.compareTo(y));
+            System.out.println("max2 ="+max2.get());
+
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+
+        LocalDate dayOfWeek = LocalDate.now();
+        LocalDate firstMonday = dayOfWeek.with(TemporalAdjusters.firstInMonth(DayOfWeek.MONDAY));
+        System.out.printf(Locale.ENGLISH,"Day of a week: %tA\nFirst monday for current month: %s\n",dayOfWeek,firstMonday);
+
+        List<String> stringList =  Arrays.asList("Kolya","Andy","Denis","Zeta");
+        System.out.println("Original :"+stringList);
+        Sort(stringList);
+        System.out.println("Sorted :"+stringList);
+    }
+}
